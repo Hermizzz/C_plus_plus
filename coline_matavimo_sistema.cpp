986d1b6e00ad7e269cc320a4b2ec2628ebec338c
Hermizzz <95226480+Hermizzz@users.noreply.github.com>
2021-11-30 09:31:18 +0200

    Create coline_matavimo_sistema.cpp
    9 uždavinys

---
+++ coline_matavimo_sistema.cpp
@@ -0,0 +1,10 @@
+/*
+
+Colinė matavimo sistema. Kai kuriose šalyse dar ir dar vartojama colinė matavimo sistema. 12 colių sudaro pėdą, o 3 pėdos lygios vienam jardui.
+Parašykite programą, skaičiuojančią dviejų skirtingų ilgių, išreikštų jardais, pėdomis ir coliais, sumą ir skirtumą, išreikštą pėdomis ir coliais.
+
+Pradiniuose duomenyse pirmieji trys skaičiai reiškia ilgio jardus j1, pėdas p1 ir colius c1, paskutinieji trys skaičiai - antrojo ilgio jardus j2, pėdas p2 ir colius c2.
+Rezultatuose pirmieji trys skaičiai reiškia sumos jardus sj, sumos colius sc; paskutinieji trys skaičiai - skirtumo jardus skj, skirtumo pėdas skp, skirtumo colius skc.
+
+*/
+
